912cbb9f7c3a66b267497366a1423ba1211972f4
Gulam Gaush <mohammadgulamgaush@gmail.com>
2021-12-29 19:15:28 +0530

    cors setup

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ const corsOptions = {
    //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(cors(corsOptions))
 app.use(express.static('public'));
 
