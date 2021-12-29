8d2cb5d2451913c8515b36499aac727ba8961beb
Gulam Gaush <mohammadgulamgaush@gmail.com>
2021-12-29 20:29:23 +0530

    again

--- routes/download.js
+++ routes/download.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const File = require('../models/file');
 
-router.get('/:uuid', async (req, res) => {
+router.get(':uuid', async (req, res) => {
    // Extract link and get file from storage send download stream 
    const file = await File.findOne({ uuid: req.params.uuid });
    // Link expired
@@ -9,7 +9,7 @@ router.get('/:uuid', async (req, res) => {
         return res.render('download', { error: 'Link has been expired.'});
    } 
    const response = await file.save();
-   const filePath = `${__dirname}/${file.path}`;
+   const filePath = `${__dirname}/../${file.path}`;
    res.download(filePath);
 });
 
