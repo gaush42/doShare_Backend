b90cafc56a49b662ef50f7e739580c796afcd76e
Gulam Gaush <mohammadgulamgaush@gmail.com>
2021-12-29 19:46:39 +0530

    cors

--- server.js
+++ server.js
@@ -4,23 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
-const corsOptions = {
 
-  //origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-   //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-app.use(cors(corsOptions))
+app.use(cors({
+  origin: 'https://do-share-frontend.vercel.app'
+}));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
